41534b9e5759facc3baae322f26f700ff7857f6c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-08-16 20:44:36 -0500

    Reinitialize project using `cobra-cli`

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,10 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, ... }:
 
 {
+  packages = [
+    pkgs.cobra-cli
+  ];
+
   languages.go.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
