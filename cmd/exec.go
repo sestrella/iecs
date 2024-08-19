c6b797ae04191534248718134d76c0bc88e32816
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-08-19 05:29:43 -0500

    Get runtimeId for each container

--- cmd/exec.go
+++ cmd/exec.go
@@ -178,9 +178,11 @@ func selectContainer(client *ecs.Client, cluster item, task item) (*item, error)
 
 	items := []list.Item{}
 	for _, container := range output.Tasks[0].Containers {
+		slices := strings.Split(*container.RuntimeId, "-")
 		items = append(items, item{
-			name: *container.Name,
-			arn:  *container.ContainerArn,
+			name:      *container.Name,
+			arn:       *container.ContainerArn,
+			runtimeId: slices[1],
 		})
 	}
 	return newSelector("Tasks", items)
