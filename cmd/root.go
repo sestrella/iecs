54aab1fc1be7983de50b593b5e4b4fc923dab0fb
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-08-24 20:35:23 -0500

    Add some TODO elements to description

--- cmd/root.go
+++ cmd/root.go
@@ -63,12 +63,7 @@ var docStyle = lipgloss.NewStyle().Margin(1, 2)
 var rootCmd = &cobra.Command{
 	Use:   "lazy-ecs",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  "TODO",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
