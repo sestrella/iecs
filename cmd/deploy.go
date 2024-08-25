6d59fa391cffe15aa71c4815f53c2d22febc441c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-08-24 21:19:13 -0500

    Add service selector

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -2,15 +2,19 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/spf13/cobra"
 )
 
 var deployClusterId string
+var deployServiceId string
 
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
@@ -27,12 +31,62 @@ var deployCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(cluster)
+
+		service, err := selectService(context.TODO(), client, *cluster)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(service)
 	},
+}
+
+func selectService(ctx context.Context, client *ecs.Client, cluster item) (*item, error) {
+	if deployServiceId != "" {
+		output, err := client.DescribeServices(ctx, &ecs.DescribeServicesInput{
+			Cluster:  &cluster.arn,
+			Services: []string{deployServiceId},
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		if len(output.Services) == 0 {
+			return nil, fmt.Errorf("Service '%s' not found", deployServiceId)
+		}
+
+		task := output.Services[0]
+		slices := strings.Split(*task.ServiceArn, "/")
+		return &item{
+			name: fmt.Sprintf("%s/%s", slices[1], slices[2]),
+			arn:  *task.ServiceArn,
+		}, nil
+	}
+
+	output, err := client.ListServices(ctx, &ecs.ListServicesInput{
+		Cluster: &cluster.arn,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(output.ServiceArns) == 0 {
+		return nil, errors.New("No services found")
+	}
+
+	items := []list.Item{}
+	for _, arn := range output.ServiceArns {
+		index := strings.LastIndex(arn, "/")
+		items = append(items, item{
+			name: arn[index+1:],
+			arn:  arn,
+		})
+	}
+	return newSelector("Services", items)
 }
 
 func init() {
 	rootCmd.AddCommand(deployCmd)
 
 	deployCmd.Flags().StringVarP(&deployClusterId, "cluster", "c", "", "TODO")
+	deployCmd.Flags().StringVarP(&deployServiceId, "service", "s", "", "TODO")
 }
