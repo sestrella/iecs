9ece68ef876b81e7c124a45aa8aeb78ddb832284
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-09-18 23:54:01 -0500

    Basic CLI

--- rust/lazy_ecs/src/main.rs
+++ rust/lazy_ecs/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "lazy-ecs")]
+enum Cli {
+    Ssh(SshArgs),
+}
+
+#[derive(clap::Args)]
+struct SshArgs {
+    #[arg(long)]
+    cluster: Option<String>,
+    #[arg(long)]
+    task: Option<String>,
+    #[arg(long)]
+    container: Option<String>,
+    #[arg(long, default_value = "/bin/sh")]
+    command: String,
+    #[arg(long, default_value_t = true)]
+    interactive: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Cli::Ssh(args) = Cli::parse();
+    println!("{:?}", args.cluster);
 }
