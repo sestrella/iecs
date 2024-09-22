7c312c5dc9f8316198bd973389e7a5697f6dd1e3
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-09-22 03:17:20 -0500

    Getting container / Improving error handling

--- rust/lazy_ecs/src/main.rs
+++ rust/lazy_ecs/src/main.rs
@@ -1,9 +1,10 @@
+use std::fmt::Display;
+
+use anyhow::Context;
 use aws_config::BehaviorVersion;
-use aws_sdk_ecs::{
-    error::SdkError,
-    operation::{describe_clusters::DescribeClustersError, list_clusters::ListClustersError},
-};
+use aws_sdk_ecs::types::Container;
 use clap::Parser;
+use inquire::Select;
 
 #[derive(Parser)]
 #[command(name = "lazy-ecs")]
@@ -25,53 +26,88 @@ struct ExecArgs {
     interactive: bool,
 }
 
-#[derive(Debug)]
-enum MyError {
-    DescribeClustersError(SdkError<DescribeClustersError>),
-    ListClustersError(SdkError<ListClustersError>),
+struct SelectableContainer {
+    name: String,
+    arn: String,
 }
 
-impl From<SdkError<DescribeClustersError>> for MyError {
-    fn from(value: SdkError<DescribeClustersError>) -> Self {
-        Self::DescribeClustersError(value)
-    }
-}
+impl TryFrom<Container> for SelectableContainer {
+    type Error = &'static str;
 
-impl From<SdkError<ListClustersError>> for MyError {
-    fn from(value: SdkError<ListClustersError>) -> Self {
-        Self::ListClustersError(value)
+    fn try_from(value: Container) -> Result<Self, Self::Error> {
+        let name = value.name.ok_or("Name not found")?;
+        let arn = value.container_arn.ok_or("ARN not found")?;
+        Ok(SelectableContainer { name, arn })
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), MyError> {
+async fn main() -> anyhow::Result<()> {
     let Cli::Exec(args) = Cli::parse();
 
     let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
     let client = aws_sdk_ecs::Client::new(&config);
     let cluster = get_cluster(&client, &args.cluster).await?;
-    println!("{:?}", cluster);
+    let task = get_task(&client, &cluster, &args.task).await?;
+    let container = get_container(&client, &cluster, &task, &args.container).await?;
+    println!("{:?}", container);
     Ok(())
 }
 
+// TODO: Improve error message
 async fn get_cluster(
     client: &aws_sdk_ecs::Client,
-    cluster_name_option: &Option<String>,
-) -> Result<aws_sdk_ecs::types::Cluster, MyError> {
-    if let Some(cluster_name) = cluster_name_option {
+    cluster_arg: &Option<String>,
+) -> anyhow::Result<String> {
+    if let Some(cluster_name) = cluster_arg {
         let output = client
             .describe_clusters()
             .clusters(cluster_name)
             .send()
             .await?;
-        if let Some(clusters) = output.clusters {
-            if let Some(cluster) = clusters.first() {
-                // TODO: try to get rid of the clone
-                return Ok(cluster.clone());
-            }
-        }
-    } else {
-        let _output = client.list_clusters().send().await?;
+        let clusters = output.clusters.context("")?;
+        let cluster = clusters.first().context("")?;
+        let cluster_arn = cluster.cluster_arn.as_ref().context("")?;
+        return Ok(cluster_arn.to_string());
     }
-    todo!()
+    let output = client.list_clusters().send().await?;
+    let clusters = output.cluster_arns.context("")?;
+    let cluster = Select::new("Cluster", clusters).prompt().context("")?;
+    Ok(cluster)
+}
+
+// TODO: take into account task_arg
+async fn get_task(
+    client: &aws_sdk_ecs::Client,
+    cluster: &String,
+    task_arg: &Option<String>,
+) -> anyhow::Result<String> {
+    let output = client.list_tasks().cluster(cluster).send().await?;
+    let tasks = output.task_arns.context("")?;
+    let task = Select::new("Task", tasks).prompt().context("")?;
+    Ok(task)
+}
+
+async fn get_container(
+    client: &aws_sdk_ecs::Client,
+    cluster: &String,
+    task: &String,
+    container_arg: &Option<String>,
+) -> anyhow::Result<String> {
+    let output = client
+        .describe_tasks()
+        .cluster(cluster)
+        .tasks(task)
+        .send()
+        .await?;
+    let containers: Vec<Result<SelectableContainer, &'static str>> = output
+        .tasks
+        .unwrap_or(Vec::new())
+        .into_iter()
+        .map(|task| task.containers.unwrap_or(Vec::new()))
+        .flatten()
+        .map(|container| SelectableContainer::try_from(container))
+        .collect();
+    // let container = Select::new("Container", containers).prompt().context("")?;
+    Ok("".to_string())
 }
