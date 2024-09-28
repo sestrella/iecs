0e779de9aa0ad16a1ef9dac69d1e65ec35a80dfe
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-09-27 22:04:44 -0500

    Remove some comments

--- cmd/root.go
+++ cmd/root.go
@@ -122,8 +122,6 @@ func newSelector(title string, items []list.Item) (*item, error) {
 	return &selected, nil
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
