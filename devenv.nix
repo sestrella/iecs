0a6b06b6a5721e50d0e82d273c6c380fdead5906
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-10-30 21:57:37 -0500

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,20 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   packages = [
     pkgs.aws-vault
     pkgs.awscli2
-    pkgs.cobra-cli
     pkgs.ssm-session-manager-plugin
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    pkgs.darwin.Security
   ];
-
-  languages.go.enable = true;
 
   languages.rust.enable = true;
   languages.rust.channel = "stable";
 
-  pre-commit.hooks.gofmt.enable = true;
-  pre-commit.hooks.golangci-lint.enable = true;
+  pre-commit.hooks.clippy.enable = true;
+  pre-commit.hooks.nixpkgs-fmt.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
