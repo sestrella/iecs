d6ed42f5ee0f906d7b7e40e638e6a5dbbf026c78
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-11-05 01:32:37 -0500

    Bootstrap cobra project

--- devenv.nix
+++ devenv.nix
@@ -4,10 +4,13 @@
   packages = [
     pkgs.aws-vault
     pkgs.awscli2
+    pkgs.cobra-cli
     pkgs.ssm-session-manager-plugin
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     pkgs.darwin.Security
   ];
+
+  languages.go.enable = true;
 
   languages.rust.enable = true;
   languages.rust.channel = "stable";
