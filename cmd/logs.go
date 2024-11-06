f1233fcaa77b7c42d58ed9833e43aa6801f5af58
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-11-06 00:40:46 -0500

    Print log mesages from a container

--- cmd/logs.go
+++ cmd/logs.go
@@ -1,12 +1,21 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	cwlogsTypes "github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
@@ -14,19 +23,76 @@ import (
 var logsCmd = &cobra.Command{
 	Use:   "logs",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("logs called")
+		cfg, _ := config.LoadDefaultConfig(context.TODO())
+
+		ecsClient := ecs.NewFromConfig(cfg)
+		cwlogsClient := cloudwatchlogs.NewFromConfig(cfg)
+		stsClient := sts.NewFromConfig(cfg)
+
+		listClusters, _ := ecsClient.ListClusters(context.TODO(), &ecs.ListClustersInput{})
+		clusterArn, _ := pterm.DefaultInteractiveSelect.WithOptions(listClusters.ClusterArns).Show("Cluster")
+		listTasks, _ := ecsClient.ListTasks(context.TODO(), &ecs.ListTasksInput{
+			Cluster: &clusterArn,
+		})
+		taskArn, _ := pterm.DefaultInteractiveSelect.WithOptions(listTasks.TaskArns).Show("Task")
+		tasks, _ := ecsClient.DescribeTasks(context.TODO(), &ecs.DescribeTasksInput{
+			Cluster: &clusterArn,
+			Tasks:   []string{taskArn},
+		})
+		task := tasks.Tasks[0]
+		taskDefinition, _ := ecsClient.DescribeTaskDefinition(context.TODO(), &ecs.DescribeTaskDefinitionInput{
+			TaskDefinition: task.TaskDefinitionArn,
+		})
+
+		var containerNames []string
+		for _, container := range taskDefinition.TaskDefinition.ContainerDefinitions {
+			containerNames = append(containerNames, *container.Name)
+		}
+		containerName, _ := pterm.DefaultInteractiveSelect.WithOptions(containerNames).Show("Container")
+		var selectedContainer *types.ContainerDefinition
+		for _, container := range taskDefinition.TaskDefinition.ContainerDefinitions {
+			if *container.Name == containerName {
+				selectedContainer = &container
+				break
+			}
+		}
+		logOptions := selectedContainer.LogConfiguration.Options
+		getCallerIdentity, _ := stsClient.GetCallerIdentity(context.TODO(), &sts.GetCallerIdentityInput{})
+
+		startLiveTail, _ := cwlogsClient.StartLiveTail(context.TODO(), &cloudwatchlogs.StartLiveTailInput{
+			LogGroupIdentifiers:   []string{fmt.Sprintf("arn:aws:logs:%s:%s:log-group:%s", logOptions["awslogs-region"], *getCallerIdentity.Account, logOptions["awslogs-group"])},
+			LogStreamNamePrefixes: []string{logOptions["awslogs-stream-prefix"]},
+		})
+		stream := startLiveTail.GetStream()
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			eventsChannel := stream.Events()
+			for {
+				event := <-eventsChannel
+				switch e := event.(type) {
+				case *cwlogsTypes.StartLiveTailResponseStreamMemberSessionStart:
+					log.Println("Received SessionStart event")
+				case *cwlogsTypes.StartLiveTailResponseStreamMemberSessionUpdate:
+					for _, logEvent := range e.Value.SessionResults {
+						log.Println(*logEvent.Message)
+					}
+				default:
+					fmt.Println("TODO")
+					return
+				}
+			}
+		}()
+		wg.Wait()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(logsCmd)
+
+	rootCmd.Flags().String("cluster", "", "TODO")
 
 	// Here you will define your flags and configuration settings.
 
