81429f0e8e083b65280ffd4413bbbbb436dc97a9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-11-12 04:56:25 -0500

    Remove Rust version :(

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   packages = [
@@ -6,18 +6,11 @@
     pkgs.awscli2
     pkgs.cobra-cli
     pkgs.ssm-session-manager-plugin
-  ] ++ lib.optionals pkgs.stdenv.isDarwin [
-    pkgs.darwin.Security
   ];
 
   languages.go.enable = true;
 
-  languages.rust.enable = true;
-  languages.rust.channel = "stable";
-
-  pre-commit.hooks.clippy.enable = true;
   pre-commit.hooks.nixpkgs-fmt.enable = true;
-  pre-commit.hooks.rustfmt.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
