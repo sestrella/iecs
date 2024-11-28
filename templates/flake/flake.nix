f5224936afa3486150dbdd0fa6ad15246c0e31d9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-11-28 00:22:07 -0500

    Remove flake-utils from flake template

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -1,19 +1,24 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     iecs.url = "github:sestrella/iecs/nix_templates";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { flake-utils, iecs, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        iecsPkgs = iecs.packages.${system};
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ iecsPkgs.default ];
-        };
-      });
+  outputs = { iecs, nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (system:
+        let
+          iecsPkgs = iecs.packages.${system};
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ iecsPkgs.default ];
+          };
+        });
+    };
 }
