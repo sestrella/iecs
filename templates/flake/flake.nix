b1fbcb0eb9dba5cdfa746b58528c9d0aca9a8deb
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-11-28 20:51:05 -0500

    Add binary cache settings to templates

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -5,6 +5,11 @@
     systems.url = "github:nix-systems/default";
   };
 
+  nixConfig = {
+    extra-substituters = "https://sestrella.cachix.org";
+    extra-trusted-public-keys = "sestrella.cachix.org-1:uf75o4yckcsAOFu6ldfPug/kTUMybvT0IY61sck2qnA=";
+  };
+
   outputs = { iecs, nixpkgs, systems, ... }:
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
