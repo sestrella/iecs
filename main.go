7f9fcd8653677091477dac71d0b72eb0a834eed2
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-04-04 21:13:10 -0500

    Call new selector from main (testing)

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/sestrella/iecs/cmd"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/sestrella/iecs/cmd"
+	"github.com/sestrella/iecs/selector"
+)
 
 func main() {
-	cmd.Execute()
+	if true {
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+		client := ecs.NewFromConfig(cfg)
+		result, err := selector.RunHuhForm(context.TODO(), client)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print(result)
+	} else {
+		cmd.Execute()
+	}
 }
