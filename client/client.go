3c9974bf83711b59163159a1263e767e241b5b6e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-04-10 03:46:41 -0500

    Add DescribeTaskDefinition to ClientV2

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
@@ -45,7 +46,7 @@ type Client interface {
 	) (*ecs.ListTasksOutput, error)
 }
 
-type NewClient interface {
+type ClientV2 interface {
 	DescribeCluster(ctx context.Context, clusterArn string) (*types.Cluster, error)
 	DescribeService(
 		ctx context.Context,
@@ -53,22 +54,29 @@ type NewClient interface {
 		serviceArn string,
 	) (*types.Service, error)
 	DescribeTask(ctx context.Context, clusterArn string, taskArn string) (*types.Task, error)
+	DescribeTaskDefinition(
+		ctx context.Context,
+		taskDefinitionArn string,
+	) (*types.TaskDefinition, error)
 	ListClusters(ctx context.Context) ([]string, error)
 	ListServices(ctx context.Context, clusterArn string) ([]string, error)
 	ListTasks(ctx context.Context, clusterArn string, serviceArn string) ([]string, error)
 }
 
 type newClient struct {
-	client Client
+	client ecs.Client
 }
 
-func NewClient(client Client) NewClient {
+func NewClientV2(client ecs.Client) ClientV2 {
 	return &newClient{
 		client: client,
 	}
 }
 
-func (c *newClient) DescribeCluster(ctx context.Context, clusterArn string) (*types.Cluster, error) {
+func (c *newClient) DescribeCluster(
+	ctx context.Context,
+	clusterArn string,
+) (*types.Cluster, error) {
 	output, err := c.client.DescribeClusters(ctx, &ecs.DescribeClustersInput{
 		Clusters: []string{clusterArn},
 	})
@@ -76,12 +84,16 @@ func (c *newClient) DescribeCluster(ctx context.Context, clusterArn string) (*ty
 		return nil, err
 	}
 	if len(output.Clusters) == 0 {
-		return nil, nil
+		return nil, fmt.Errorf("cluster not found: %s", clusterArn)
 	}
 	return &output.Clusters[0], nil
 }
 
-func (c *newClient) DescribeService(ctx context.Context, clusterArn string, serviceArn string) (*types.Service, error) {
+func (c *newClient) DescribeService(
+	ctx context.Context,
+	clusterArn string,
+	serviceArn string,
+) (*types.Service, error) {
 	output, err := c.client.DescribeServices(ctx, &ecs.DescribeServicesInput{
 		Cluster:  &clusterArn,
 		Services: []string{serviceArn},
@@ -90,12 +102,16 @@ func (c *newClient) DescribeService(ctx context.Context, clusterArn string, serv
 		return nil, err
 	}
 	if len(output.Services) == 0 {
-		return nil, nil
+		return nil, fmt.Errorf("service not found: %s", serviceArn)
 	}
 	return &output.Services[0], nil
 }
 
-func (c *newClient) DescribeTask(ctx context.Context, clusterArn string, taskArn string) (*types.Task, error) {
+func (c *newClient) DescribeTask(
+	ctx context.Context,
+	clusterArn string,
+	taskArn string,
+) (*types.Task, error) {
 	output, err := c.client.DescribeTasks(ctx, &ecs.DescribeTasksInput{
 		Cluster: &clusterArn,
 		Tasks:   []string{taskArn},
@@ -104,15 +120,34 @@ func (c *newClient) DescribeTask(ctx context.Context, clusterArn string, taskArn
 		return nil, err
 	}
 	if len(output.Tasks) == 0 {
-		return nil, nil
+		return nil, fmt.Errorf("task not found: %s", taskArn)
 	}
 	return &output.Tasks[0], nil
+}
+
+func (c *newClient) DescribeTaskDefinition(
+	ctx context.Context,
+	taskDefinitionArn string,
+) (*types.TaskDefinition, error) {
+	output, err := c.client.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: &taskDefinitionArn,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if output.TaskDefinition == nil {
+		return nil, fmt.Errorf("task definition not found: %s", taskDefinitionArn)
+	}
+	return output.TaskDefinition, nil
 }
 
 func (c *newClient) ListClusters(ctx context.Context) ([]string, error) {
 	output, err := c.client.ListClusters(ctx, &ecs.ListClustersInput{})
 	if err != nil {
 		return nil, err
+	}
+	if len(output.ClusterArns) == 0 {
+		return []string{}, fmt.Errorf("no clusters found")
 	}
 	return output.ClusterArns, nil
 }
@@ -124,16 +159,30 @@ func (c *newClient) ListServices(ctx context.Context, clusterArn string) ([]stri
 	if err != nil {
 		return nil, err
 	}
+	if len(output.ServiceArns) == 0 {
+		return []string{}, fmt.Errorf("no services found in cluster: %s", clusterArn)
+	}
 	return output.ServiceArns, nil
 }
 
-func (c *newClient) ListTasks(ctx context.Context, clusterArn string, serviceArn string) ([]string, error) {
+func (c *newClient) ListTasks(
+	ctx context.Context,
+	clusterArn string,
+	serviceArn string,
+) ([]string, error) {
 	output, err := c.client.ListTasks(ctx, &ecs.ListTasksInput{
 		Cluster:     &clusterArn,
 		ServiceName: &serviceArn,
 	})
 	if err != nil {
 		return nil, err
+	}
+	if len(output.TaskArns) == 0 {
+		return []string{}, fmt.Errorf(
+			"no tasks found for service: %s in cluster: %s",
+			serviceArn,
+			clusterArn,
+		)
 	}
 	return output.TaskArns, nil
 }
