d67574c9bb0e52459a85c8a5b4058c41589d5ad8
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-06-25 22:11:02 -0500

    feat: Add PagesWidget

---
+++ internal/ui/pages.go
@@ -0,0 +1,23 @@
+package ui
+
+import "github.com/rivo/tview"
+
+type PagesWidget struct {
+	*tview.Pages
+	titlesPerPage map[string]string
+}
+
+func NewPagesWidget(title string) *PagesWidget {
+	view := tview.NewPages()
+	view.SetTitle(title)
+	view.SetBorder(true)
+	return &PagesWidget{
+		Pages:         view,
+		titlesPerPage: make(map[string]string),
+	}
+}
+
+func (widget *PagesWidget) AddPage(name string, title string, page tview.Primitive) {
+	widget.titlesPerPage[name] = title
+	widget.AddAndSwitchToPage(name, page, true)
+}
