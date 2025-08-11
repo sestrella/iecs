547d80ccd7afad7bef61b65796f66fba37911319
davidnbr <davidnbr_98@hotmail.com>
2025-08-11 00:39:22 -0400

    chore: add mock test for /bin/sh

--- cmd/exec/root_test.go
+++ cmd/exec/root_test.go
@@ -82,6 +82,76 @@ func TestRunExec_Success(t *testing.T) {
 	mockClient.AssertExpectations(t)
 }
 
+func TestRunExec_SuccessSh(t *testing.T) {
+	// Create mock objects
+	mockSel := new(MockSelectors)
+	mockClient := new(MockClient)
+
+	// Setup mock responses
+	clusterArn := "arn:aws:ecs:us-east-1:123456789012:cluster/my-cluster"
+	clusterName := "my-cluster"
+	serviceArn := "arn:aws:ecs:us-east-1:123456789012:service/my-cluster/my-service"
+	taskArn := "arn:aws:ecs:us-east-1:123456789012:task/my-cluster/12345678-1234-1234-1234-123456789012"
+	containerName := "my-container"
+	containerRuntimeId := "12345678abcdef"
+
+	// Mock cluster
+	cluster := &types.Cluster{
+		ClusterArn:  &clusterArn,
+		ClusterName: &clusterName,
+	}
+
+	// Mock service
+	service := &types.Service{
+		ServiceArn: &serviceArn,
+	}
+
+	// Mock container
+	container := &types.Container{
+		Name:      &containerName,
+		RuntimeId: &containerRuntimeId,
+	}
+
+	// Mock task
+	task := &types.Task{
+		TaskArn: &taskArn,
+		Containers: []types.Container{
+			*container,
+		},
+	}
+
+	// Setup mock calls for selectors
+	mockSel.On("Cluster", mock.Anything).Return(cluster, nil)
+	mockSel.On("Service", mock.Anything, cluster).Return(service, nil)
+	mockSel.On("Task", mock.Anything, service).Return(task, nil)
+	mockSel.On("Container", mock.Anything, task.Containers).Return(container, nil)
+
+	// Mock ExecuteCommand response
+
+	mockClient.On("ExecuteCommand",
+		mock.Anything,
+		cluster,
+		*task.TaskArn,
+		container,
+		"/bin/sh -i",
+		true,
+	).Return(exec.Command("echo"), nil)
+
+	// Test the function
+	err := runExec(
+		context.Background(),
+		mockClient,
+		mockSel,
+		"/bin/sh",
+		true,
+	)
+
+	// Check assertions
+	assert.NoError(t, err)
+	mockSel.AssertExpectations(t)
+	mockClient.AssertExpectations(t)
+}
+
 func TestRunExec_ClusterSelectorError(t *testing.T) {
 	// Create mock objects
 	mockSel := new(MockSelectors)
